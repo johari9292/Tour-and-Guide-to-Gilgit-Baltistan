6e5dc86f143c51098e694b67c04e1a7235be5495
Ishaq Ali <johari9292@gmail.com>
2018-12-07 19:14:13 +0500

    Add project files.

---
+++ Tour and Guide to GilgitBaltistan/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tour_and_Guide_to_GilgitBaltistan
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Tour and Guide to GilgitBaltistan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tour_and_Guide_to_GilgitBaltistan.Startup))]
+namespace Tour_and_Guide_to_GilgitBaltistan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
